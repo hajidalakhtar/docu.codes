c3ec23fe064992f59d2024ac816557128d1590f4
Hajidalakhtar <hajidalakhtar@gmail.com>
2022-02-03 06:24:43 +0000

    add list

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1600dd8093f04c878a2"; }
+/******/ 		__webpack_require__.h = function() { return "5165b58d13d03da37ed8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
